4b00ff60cc329d9f688e472ecdde10dc8ca53fcd
Divuzki <divuzki@gmail.com>
2025-02-18 23:22:38 +0100

    Fixed AWS StaticFILES Issue

--- server/storage_backends.py
+++ server/storage_backends.py
@@ -1,24 +1,38 @@
 from django.conf import settings
-from storages.backends.azure_storage import AzureStorage
-import re
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class CachedS3BotoStorage(S3Boto3Storage):
+    """
+    S3BotoStorage backend which also saves a hashed copies of the files it saves.
+    """
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+    custom_domain = settings.STATIC_URL
 
 
-class AzureMediaStorage(AzureStorage):
-    account_name = settings.AZURE_ACCOUNT_NAME
-    account_key = settings.AZURE_ACCOUNT_KEY
-    azure_container = "media"
-    expiration_secs = None
-
-    def url(self, name):
-        blob_url = super(AzureMediaStorage, self).url(name)
-        imagekit_url = re.sub(
-            r"https://[a-z.0-9A-Z]*", settings.MEDIA_URL, blob_url.replace("media/", "")
-        )
-        return imagekit_url.replace("media//", "media/")
+class MediaS3BotoStorage(S3Boto3Storage):
+    """
+    S3BotoStorage backend for media files.
+    """
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+    custom_domain = settings.MEDIA_URL
 
 
-class AzureStaticStorage(AzureStorage):
-    account_name = settings.AZURE_ACCOUNT_NAME
-    account_key = settings.AZURE_ACCOUNT_KEY
-    azure_container = "static"
-    expiration_secs = None
+class StaticStorage(S3Boto3Storage):
+    location = settings.STATIC_LOCATION
+    default_acl = 'public-read'
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN
+
+
+class PublicMediaStorage(S3Boto3Storage):
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+    # custom_domain = settings.MEDIA_URL
+    location = settings.PUBLIC_MEDIA_LOCATION
+    default_acl = 'public-read'
+    file_overwrite = True
+
+# class PrivateMediaStorage(S3Boto3Storage):
+#     location = settings.PRIVATE_MEDIA_LOCATION
+#     default_acl = 'private'
+#     file_overwrite = False
+#     custom_domain = False
