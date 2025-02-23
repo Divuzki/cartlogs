2d06ffbb5b781f31331c2a589ad8a12d4ddd4fed
Divuzki <divuzki@gmail.com>
2025-02-23 12:33:28 +0100

    Updated core/admins.py

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,7 @@ from .models import Wallet, Transaction
 
 class TransactionInline(admin.TabularInline):
     model = Transaction
-    readonly_fields = ('wallet', 'amount', 'type', 'created_at')
+    readonly_fields = ('payment_reference', 'payment_gateway', 'wallet', 'amount', 'type', 'description', 'created_at')
     extra = 0
 
 class WalletAdmin(admin.ModelAdmin):
