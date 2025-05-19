04b43f41592c48d3eaa1cfadda26531acd93dc16
Divuzki <divuzki@gmail.com>
2025-05-19 19:50:36 +0100

    fix(settings): correct DEBUG condition for security settings
    Ensure security settings like CSRF_COOKIE_SECURE and SECURE_SSL_REDIRECT are correctly applied when DEBUG is False. This prevents potential security vulnerabilities in production environments.

--- server/settings.py
+++ server/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n^pi7+zdv-t9w!gc2707%206rh&o%4q5_x&f5vzc7pohgq(k76'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG", "False") != "True"
 USE_S3 = os.getenv("USE_S3", "False") == "True"
 
 ALLOWED_HOSTS = ["*"]
@@ -40,14 +40,14 @@ PAYMENT_GATEWAYS = {
 KORAPAY_SECRET_KEY = os.environ.get('KORAPAY_SECRET_KEY')
 KORAPAY_PUBLIC_KEY = os.environ.get('KORAPAY_PUBLIC_KEY')
 
-CSRF_COOKIE_SECURE = DEBUG == False
-SESSION_COOKIE_SECURE = DEBUG == False
-SECURE_SSL_REDIRECT = DEBUG == False
-SECURE_HSTS_SECONDS = 518400 if (DEBUG == False) else None
-SECURE_HSTS_INCLUDE_SUBDOMAINS = DEBUG == False
-SECURE_HSTS_PRELOAD = DEBUG == False
+CSRF_COOKIE_SECURE = DEBUG != False
+SESSION_COOKIE_SECURE = DEBUG != False
+SECURE_SSL_REDIRECT = DEBUG != False
+SECURE_HSTS_SECONDS = 518400 if (DEBUG != False) else None
+SECURE_HSTS_INCLUDE_SUBDOMAINS = DEBUG != False
+SECURE_HSTS_PRELOAD = DEBUG != False
 SECURE_PROXY_SSL_HEADER = (
-    ("HTTP_X_FORWARDED_PROTO", "https") if (DEBUG == False) else None
+    ("HTTP_X_FORWARDED_PROTO", "https") if (DEBUG != False) else None
 )
 
 # CSRF and CORS Settings
